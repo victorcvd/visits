f8668d9425b04ef96211c418b6f943d6445631d1
Victor Cuevas <vcuevas@mq.cl>
2023-07-20 18:42:48 -0400

    enviroment variables added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+    region: "us-east-1", 
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const params = {
+            TableName : "brokers",
+        };
+
+        docClient.scan(params, function(err, data) {
+            if (err) {
+                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                setData(data.Items);
+                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            }
+        });
+    }, []);
+
+    return (
+        <div>
+            {data.map(item => (
+                <div key={item.id}>{item.name}</div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
+
